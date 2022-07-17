d59f95a7d5ddaf6e68cc20e33e23743d8e40d2ec
Lucian Danescu <lucidanescu28@yahoo.com>
2022-07-17 22:07:45 +0300

    remove comments

--- src/dscanner/analysis/run.d
+++ src/dscanner/analysis/run.d
@@ -478,10 +478,6 @@ MessageSet analyze(string fileName, const Module m, const StaticAnalysisConfig a
 		checks ~= new CommaExpressionCheck(fileName, moduleScope,
 		analysisConfig.comma_expression_check == Check.skipTests && !ut);
 
-	// if (moduleName.shouldRun!ConstructorCheck(analysisConfig))
-	// 	checks ~= new ConstructorCheck(fileName, moduleScope,
-	// 	analysisConfig.constructor_check == Check.skipTests && !ut);
-
 	if (moduleName.shouldRun!UnmodifiedFinder(analysisConfig))
 		checks ~= new UnmodifiedFinder(fileName, moduleScope,
 		analysisConfig.could_be_immutable_check == Check.skipTests && !ut);
