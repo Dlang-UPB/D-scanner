24b2f7834222484d112233d6c80112b673192960
Lucian Danescu <lucidanescu28@yahoo.com>
2023-05-28 19:32:06 +0300

    delete duplicate_attribute visitor

--- src/dscanner/analysis/run.d
+++ src/dscanner/analysis/run.d
@@ -40,7 +40,6 @@ import dscanner.analysis.constructors;
 import dscanner.analysis.unused_variable;
 import dscanner.analysis.unused_label;
 import dscanner.analysis.unused_parameter;
-import dscanner.analysis.duplicate_attribute;
 import dscanner.analysis.opequals_without_tohash;
 import dscanner.analysis.length_subtraction;
 import dscanner.analysis.builtin_property_names;
@@ -475,10 +474,6 @@ MessageSet analyze(string fileName, const Module m, const StaticAnalysisConfig a
 	if (moduleName.shouldRun!UnmodifiedFinder(analysisConfig))
 		checks ~= new UnmodifiedFinder(fileName, moduleScope,
 		analysisConfig.could_be_immutable_check == Check.skipTests && !ut);
-
-	if (moduleName.shouldRun!DuplicateAttributeCheck(analysisConfig))
-		checks ~= new DuplicateAttributeCheck(fileName, moduleScope,
-		analysisConfig.duplicate_attribute == Check.skipTests && !ut);
 
 	if (moduleName.shouldRun!FunctionAttributeCheck(analysisConfig))
 		checks ~= new FunctionAttributeCheck(fileName, moduleScope,
