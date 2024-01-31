0f38ac79347e30fed3903e6137d2605334929aab
Vladiwostok <rickvsprot@protonmail.com>
2024-01-31 20:55:40 +0200

    Refactor RedundantParenCheck to re-lex the file instead of using Expression.parens

--- src/dscanner/analysis/redundant_parens.d
+++ src/dscanner/analysis/redundant_parens.d
@@ -10,39 +10,76 @@ import dscanner.analysis.base;
 /**
  * Checks for redundant parenthesis
  */
-extern(C++) class RedundantParenCheck(AST) : BaseAnalyzerDmd
+extern (C++) class RedundantParenCheck(AST) : BaseAnalyzerDmd
 {
 	alias visit = BaseAnalyzerDmd.visit;
 	mixin AnalyzerInfo!"redundant_parens_check";
 
-	///
-	extern(D) this(string fileName, bool skipTests = false)
+	private enum string KEY = "dscanner.suspicious.redundant_parens";
+	private enum string MESSAGE = "Redundant parenthesis.";
+
+	extern (D) this(string fileName, bool skipTests = false)
 	{
 		super(fileName, skipTests);
 	}
 
-	override void visit(AST.IfStatement s)
+	override void visit(AST.IfStatement ifStatement)
 	{
-		if (s.condition.parens)
-			addErrorMessage(cast(ulong) s.loc.linnum, cast(ulong) s.loc.charnum,
-							KEY, MESSAGE);
-	}
+		import dscanner.utils : readFile;
+		import dmd.errorsink : ErrorSinkNull;
+		import dmd.globals : global;
+		import dmd.lexer : Lexer;
+		import dmd.tokens : TOK;
 
-private:
-	enum string KEY = "dscanner.suspicious.redundant_parens";
-	enum string MESSAGE = "Redundant parenthesis.";
+		__gshared ErrorSinkNull errorSinkNull;
+		if (!errorSinkNull)
+			errorSinkNull = new ErrorSinkNull;
+
+		auto bytes = readFile(fileName) ~ '\0';
+		bytes = bytes[ifStatement.loc.fileOffset .. $];
+
+		scope lexer = new Lexer(null, cast(char*) bytes, 0, bytes.length, 0, 0, errorSinkNull, &global.compileEnv);
+		while (lexer.token.value != TOK.leftParenthesis)
+			lexer.nextToken();
+
+		lexer.nextToken();
+		if (lexer.token.value != TOK.leftParenthesis)
+			return;
+
+		int openParensCount = 2;
+		while (openParensCount > 1)
+		{
+			lexer.nextToken();
+
+			switch (lexer.token.value)
+			{
+			case TOK.rightParenthesis:
+				openParensCount--;
+				break;
+			case TOK.leftParenthesis:
+				openParensCount++;
+				break;
+			default:
+				break;
+			}
+		}
+
+		lexer.nextToken();
+		if (lexer.token.value == TOK.rightParenthesis)
+			addErrorMessage(cast(ulong) ifStatement.loc.linnum, cast(ulong) ifStatement.loc.charnum, KEY, MESSAGE);
+	}
 }
 
 unittest
 {
 	import dscanner.analysis.config : StaticAnalysisConfig, Check, disabledConfig;
+	import dscanner.analysis.helpers : assertAnalyzerWarningsDMD;
 	import std.stdio : stderr;
-	import dscanner.analysis.helpers : assertAnalyzerWarnings = assertAnalyzerWarningsDMD;
 
 	StaticAnalysisConfig sac = disabledConfig();
 	sac.redundant_parens_check = Check.enabled;
 
-	assertAnalyzerWarnings(q{
+	assertAnalyzerWarningsDMD(q{
 		void testRedundantParens()
 		{
 			int a = 0;
@@ -51,7 +88,7 @@ unittest
 			if ((a + 2 == 3)) // [warn]: Redundant parenthesis.
 			{
 
-			} 
+			}
 
 			if ((b)) // [warn]: Redundant parenthesis.
 			{
@@ -61,6 +98,8 @@ unittest
 			if (b) { }
 
 			if (a * 2 == 0) { }
+
+			if ((a + 2) == (3 + 5)) { }
 		}
 	}c, sac);
 
