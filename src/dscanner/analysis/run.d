0a11c84993a59cc3f389349d1e3be3617b365006
Vladiwostok <rickvsprot@protonmail.com>
2024-10-08 18:11:36 +0300

    Replace libdparse with DMD in FunctionAttributeCheck

--- src/dscanner/analysis/run.d
+++ src/dscanner/analysis/run.d
@@ -655,10 +655,6 @@ BaseAnalyzer[] getAnalyzersForModuleAndConfig(string fileName,
 		moduleScope
 	);
 
-	if (moduleName.shouldRun!FunctionAttributeCheck(analysisConfig))
-		checks ~= new FunctionAttributeCheck(args.setSkipTests(
-		analysisConfig.function_attribute_check == Check.skipTests && !ut));
-
 	return checks;
 }
 
